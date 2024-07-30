46c041562deb7ea3ce39411267298f73bd405b8f
Riktha Reddy <139069219+Riktha-reddy@users.noreply.github.com>
2024-07-30 21:54:49 +0530

    Create guess_number_higher_or_lower.py

---
+++ guess_number_higher_or_lower.py
@@ -0,0 +1,9 @@
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        s=1
+        while s <= n:
+            m = (n + s) // 2         
+            g = guess(m)              
+            if g == 0 : return m      
+            if g < 0  : n = m - 1    
+            if g > 0  : s = m + 1
